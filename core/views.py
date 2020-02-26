2b02280f2fbea8adc366bcb6266e997afa3d945c
Brandon <bmiller3822@gmail.com>
2020-02-26 09:55:36 -0500

    Just getting caught up on Git

--- core/views.py
+++ core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import data
+from .models import Note
 
-# Create your views here.
-def index(request):
-    notes = data.NOTES
-    return render(request, 'base.html', {'notes': notes})
-    # return HttpResponse("Hello, world. You're at the core index.")
+def notes_list(request):
+    notes = Note.objects.all() #We need to do something a lot, i.e. get all objects, so here's a method
+    return render(request, 'core/notes_list.html', {'notes': notes})
+
+def notes_detail(request, pk):
+    note = Note.objects.get(pk=pk) #primary key
+    return render (request, 'core/notes_detail.html', {'note': note})
+
+
+  # return HttpResponse("Hello, world. You're at the core index.")
